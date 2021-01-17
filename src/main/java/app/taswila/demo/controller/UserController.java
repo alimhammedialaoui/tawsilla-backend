d4f1405c346d7ce2e37176c2369fc4dae3da16ae
alilou1998 <alimalaoui3@gmail.com>
2021-01-17 23:30:39 +0100

    commit

--- src/main/java/app/taswila/demo/controller/UserController.java
+++ src/main/java/app/taswila/demo/controller/UserController.java
@@ -3,9 +3,12 @@ package app.taswila.demo.controller;
 import app.taswila.demo.model.Utilisateur;
 import app.taswila.demo.repo.RepoUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,6 +18,11 @@ public class UserController {
     @Autowired
     public UserController(RepoUser repoUser) {
         this.repoUser = repoUser;
+    }
+
+    @GetMapping("/users")
+    public List<Utilisateur> users(){
+        return repoUser.findAll();
     }
 
     @PostMapping("/addUser")
