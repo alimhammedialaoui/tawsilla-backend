3b747cb822f6d1b287ea57fa4b538772ccb8148e
alilou1998 <alimalaoui3@gmail.com>
2021-01-18 21:58:39 +0100

    commit

--- src/main/java/app/taswila/demo/DemoApplication.java
+++ src/main/java/app/taswila/demo/DemoApplication.java
@@ -35,8 +35,8 @@ public class DemoApplication implements CommandLineRunner {
 
         List<String> t1 = new ArrayList<>(Arrays.asList("17:00","17:30","18:00","18:30","22:00"));
         List<String> t2 = new ArrayList<>(Arrays.asList("17:55","18:25",null,null,null));
-        List<String> t3 = new ArrayList<>(Arrays.asList("17:30","18:20",null,null,null));
-        List<String> t4 = new ArrayList<>(Arrays.asList("17:05","18:00",null,null,null));
+        List<String> t3 = new ArrayList<>(Arrays.asList(null,null,"17:30","18:20",null));
+        List<String> t4 = new ArrayList<>(Arrays.asList(null,null,null,"17:05","18:00"));
         List<String> t5 = new ArrayList<>(Arrays.asList("22:00","22:30","23:00","23:25",null));
 
         AvailableTransport transport1 = new AvailableTransport("Train","21/11/2021","17:00","22:00","Casablanca","Tanger",t1);
